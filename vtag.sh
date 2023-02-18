07735475060b0ef0d8bb4cfa363c88976fad29f0
Tommi Muona <43796235+muonato@users.noreply.github.com>
2023-02-18 10:13:23 +0200

    Tidying up

--- vtag.sh
+++ vtag.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
+# muonato/vtag @ GitHub (AUG-2021)
 # 
 # Adds running time overlay to video file, using
 # date argument or filesystem timestamp of video
-#        muonato/vtag @ GitHub (AUG-2021)
 #
 # Parameters: vtag.sh <filename> [start time]
 #	1: Path to input video file
 #	2: Start time in format dd/mm/yyyy hh:mm:ss
 #		- Using file timestamp when omitted
-# Usage:
+# Examples:
 #	vtag.sh my_video.avi 17/02/2023\ 14:50:00
+#	= starts overlay from 17-FEB-2023 at 14:50
+#
+#	vtag.sh my_video.avi
+#	= starts overlay from file modified time
 #
 FONTSIZE=64
 FONTCOLOR=yellow
@@ -17,7 +21,7 @@ FONTCOLOR=yellow
 is_ffmpeg=$(ffmpeg -version|grep -o "ffmpeg")
 
 if [[ -z $is_ffmpeg ]]; then
-    echo "missing ffmpeg, maybe try 'apt install ffmpeg' ?"
+    echo "missing ffmpeg, try 'apt install ffmpeg' maybe ?"
     exit
 fi
 
@@ -28,18 +32,22 @@ else
 fi
 
 if [[ -z $is_video ]]; then
-    echo "maybe not a video ?"
+    echo "missing video header"
     exit
 fi
 
 if [[ -z $2 ]]; then
     epoch=$(stat -c %Y $1)
-    echo "Adding filesys timestamp overlay on '$1'"
+    echo "adding filesys timestamp overlay on '$1'"
 else
     epoch=$(date -d "$2" +"%s")
-    echo "Adding video start @ '$2' overlay on '$1'"
+    echo "adding video start @ '$2' overlay on '$1'"
 fi
 
 sleep 3
 
-ffmpeg -y -i $1 -vf drawtext="fontsize=$FONTSIZE: text='%{pts\:localtime\:$epoch}':fontcolor=$FONTCOLOR" vtag_${1}
+ffmpeg -y -i $1 -vf drawtext="\
+	fontsize=$FONTSIZE:\
+	fontcolor=$FONTCOLOR:\
+	text='%{pts\:localtime\:$epoch}'"\
+	vtag_${1}
