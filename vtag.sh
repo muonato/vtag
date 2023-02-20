9f063ef0f9ce2745c8c7d89d473c39d614e96c81
Tommi Muona <43796235+muonato@users.noreply.github.com>
2023-02-20 11:04:30 +0200

    Update vtag.sh

--- vtag.sh
+++ vtag.sh
@@ -9,7 +9,7 @@
 #	2: Start time in format dd/mm/yyyy hh:mm:ss
 #		- Using file timestamp when omitted
 # Examples:
-#	vtag.sh my_video.avi 17/02/2023\ 14:50:00
+#	vtag.sh my_video.avi 02/17/2023\ 14:50:00
 #	= starts overlay from 17-FEB-2023 at 14:50
 #
 #	vtag.sh my_video.avi
